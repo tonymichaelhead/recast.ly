5deae66e81b6888ef9839be19da12061cfbf1fb9
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-12 18:50:34 -0700

    Implement ListEntryVideo play on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={exampleVideoData[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData}/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentVideo: exampleVideoData[0]
+    };
+  }
+  click(video) {
+    this.setState({
+      currentVideo: video
+    }); 
+
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={exampleVideoData} click={this.click.bind(this)}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
