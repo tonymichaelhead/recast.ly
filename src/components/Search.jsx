ca967bac89e9dc669531ca54e6a7c05a03f41bf9
tonymichaelhead <tonymichaelhead@gmail.com>
2017-07-13 16:29:31 -0700

    Make some progress

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,29 @@
-var Search = (props) => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" onChange={e => {
-      props.onType();
-    }}/>
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChangeOnType = this.handleChangeOnType.bind(this);
+  }
+
+  handleChangeOnType(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="search-bar form-inline">
+        <input className="form-control" type="text" onChange={e => {
+          this.props.onType();
+        }}/>
+        <button className="btn hidden-sm-down">
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div> 
+    );  
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
